46ec0e739bf310d370da9e7175f75d6efa663733
Cecchy2 <dariocecchinato@gmail.com>
2024-12-12 12:35:09 +0100

    aggiunti effetti allo scroll

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,58 +1,48 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Image } from "react-bootstrap";
 
 const HomePage = () => {
-  const [isSecondSectionVisible, setIsSecondSectionVisible] = useState(false);
-  const secondSectionRef = useRef<HTMLDivElement | null>(null);
+  const [isFirstSectionVisible, setIsFirstSectionVisible] = useState(true);
 
   useEffect(() => {
-    // Configurazione IntersectionObserver con threshold
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          setIsSecondSectionVisible(entry.isIntersecting);
-        });
-      },
-      { threshold: 0.5 } // Corretto il posizionamento del parametro
-    );
-
-    if (secondSectionRef.current) {
-      observer.observe(secondSectionRef.current);
-    }
-
+    const handleScroll = () => {
+      setIsFirstSectionVisible(window.scrollY <= 150);
+    };
+    const onScroll = () => {
+      requestAnimationFrame(handleScroll);
+    };
+    window.addEventListener("scroll", onScroll);
     return () => {
-      if (secondSectionRef.current) {
-        observer.unobserve(secondSectionRef.current);
-      }
+      window.removeEventListener("scroll", onScroll);
     };
   }, []);
 
   return (
     <>
-      <div className={`section ${isSecondSectionVisible ? "opacity" : ""}`}>
-        <div className="d-flex justify-content-center align-items-center sectionImage">
+      <div className={`section ${isFirstSectionVisible ? "" : "opacityTransition"}`}>
+        <div
+          className={`d-flex justify-content-center align-items-center sectionImage ${
+            isFirstSectionVisible ? "" : "position-fixed"
+          }`}
+        >
           <div>
             <p className="m-0 fs-3 fw-bold">Ciao,</p>
             <div>
               <h1 className="m-0 display-3 fw-bold">Sono Dario Cecchinato</h1>
             </div>
           </div>
-          <Image
-            src="../../Fotoprofilo.jpg"
-            className={`profileImage object-fit-cover bordiImage z-index-100 ${
-              isSecondSectionVisible ? "fixedImage" : ""
-            }`}
-          />
+          <Image src="../../Fotoprofilo.jpg" className={`profileImage object-fit-cover bordiImage`} />
         </div>
       </div>
-      <div ref={secondSectionRef} className="section bg-dark">
-        <h1>sezione2</h1>
+
+      <div className="section bg-dark">
+        <h1>Sezione 2</h1>
       </div>
       <div className="section bg-dark">
-        <h1>sezione3</h1>
+        <h1>Sezione 3</h1>
       </div>
       <div className="section bg-dark">
-        <h1>sezione4</h1>
+        <h1>Sezione 4</h1>
       </div>
     </>
   );
