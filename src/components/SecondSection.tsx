9e01f0c428d05e2793984941758f7d3e46289e82
Cecchy2 <dariocecchinato@gmail.com>
2025-01-07 13:50:31 +0100

    cambiato coleore background secondsection e centrata posizione sezione1

--- src/components/SecondSection.tsx
+++ src/components/SecondSection.tsx
@@ -1,8 +1,8 @@
 const SecondSection = () => {
   return (
     <div className="stickyWrapper">
-      <div className="bg-dark secondSection">
-        <h1 className="text-white pt-5 text-center display-3 fw-bold">About me:</h1>
+      <div className="secondSection">
+        <h1 className=" pt-5 text-center display-3 fw-bold">About me:</h1>
       </div>
     </div>
   );
