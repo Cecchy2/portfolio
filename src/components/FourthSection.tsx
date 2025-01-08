d5cf527dfa235df9598f797cc0500006c4909e8c
Cecchy2 <dariocecchinato@gmail.com>
2025-01-08 11:08:23 +0100

    aggiunte cards a tecnologia, immagine alla fourthSection

--- src/components/FourthSection.tsx
+++ src/components/FourthSection.tsx
@@ -4,20 +4,26 @@ const FourthSection = () => {
   return (
     <>
       <div className=" bg-dark">
-        <h1 className=" pt-5 text-center display-3 fw-bold mb-4 text-white">Chi sono:</h1>
         <Container>
           <Row className="align-items-center">
             <Col md={6}>
-              <Image src="/public/Fotoprofilo.jpg" className="aboutImage mb-md-5" />
+              <Image src="/public/Fotoprofilo.jpg" className="aboutImage mb-md-5 mt-5" />
             </Col>
             <Col md={6} className="text-white">
               {" "}
+              <h1 className="  text-center display-3 fw-bold mb-2 text-white">Chi sono:</h1>
               <p className="fs-5 mb-5 mt-2">
-                Sono un Full-Stack Developer con una forte passione per la tecnologia e la programmazione. Dopo aver
-                lavorato per anni nel settore commerciale ho deciso di interrompere la mia carriera per intraprenderne
-                una nuova in un ambito che da sempre mi affascina e mi stimola a creare soluzioni innovative. Così ho
-                completato un Bootcamp di oltre 1200 ore presso Epicode, dove, grazie alla mia determinazione e allo
-                svolgimento di progetti reali, mi sono diplomato con altissimi voti.
+                Sono un Full-Stack Developer con una forte passione per la tecnologia e la programmazione. Dopo anni nel
+                settore commerciale, ho deciso di inseguire il mio vero interesse avviando una nuova carriera in un
+                ambito che mi ha sempre affascinato. Per farlo, ho completato con successo un Bootcamp intensivo di
+                oltre 1200 ore presso Epicode, dove, grazie a progetti reali e alla mia determinazione, ho consolidato
+                competenze in{" "}
+                <span className="text-warning">JavaScript, Typescript, React, Java, Spring e molto altro </span>. Oggi
+                metto insieme la mia esperienza pregressa nella gestione e nel problem solving con le mie abilità
+                tecniche per sviluppare soluzioni innovative, efficienti e focalizzate sull’utente. Sono costantemente
+                alla ricerca di nuove sfide e crescita professionale, entusiasta di collaborare con team dinamici e
+                contribuire attivamente a progetti ambiziosi. L’obiettivo? Creare prodotti digitali di valore, portando
+                passione e precisione in ogni riga di codice.
               </p>
             </Col>
           </Row>
--- src/components/SecondSection.tsx
+++ src/components/SecondSection.tsx
@@ -7,36 +7,53 @@ const SecondSection = () => {
       <div className="secondSection">
         <h1 className=" pt-5 text-center display-3 fw-bold mb-4">Le tecnologie che uso:</h1>
         <Container>
-          <Row>
-            <Col className="d-flex align-items-center ">
-              <Image src="/public/javascript.png" className="javascriptImage me-4" />
-              <h4>Javascript per lo sviluppo di applicazioni web interattive e robuste</h4>
+          <Row className="gy-4 justify-content-between">
+            <Col xs={12} sm={6} md={3} lg={2}>
+              <div className="tech-card">
+                <img src="/public/javascript.png" alt="JavaScript" className="tech-image" />
+                <div className="tech-overlay">
+                  <h4>JavaScript per lo sviluppo di applicazioni web interattive e robuste</h4>
+                </div>
+              </div>
             </Col>
-          </Row>
-          <Row>
-            <Col className="d-flex align-items-center ">
-              <Image src="/public/typescript.png" className="typescriptImage me-4" />
-              <h4 className="typescriptText">Typescript per un maggiore controllo del codice</h4>
+
+            <Col xs={12} sm={6} md={3} lg={2}>
+              <div className="tech-card">
+                <img src="/public/typescript.png" alt="TypeScript" className="tech-image" />
+                <div className="tech-overlay">
+                  <h4>TypeScript per un maggiore controllo del codice</h4>
+                </div>
+              </div>
             </Col>
-          </Row>
-          <Row>
-            <Col className="d-flex align-items-center ">
-              <Image src="/public/react.png" className="reactImage me-4" />
-              <h4>React per la creazione di interfacce utente dinamiche e performanti</h4>
+            <Col xs={12} sm={6} md={3} lg={2}>
+              <div className="tech-card">
+                <img src="/public/react.png" alt="React" className="tech-image" />
+                <div className="tech-overlay">
+                  <h4>React per la creazione di interfacce utente dinamiche e performanti</h4>
+                </div>
+              </div>
             </Col>
-          </Row>
-          <Row>
-            <Col className="d-flex align-items-center justify-content-center">
-              <Image src="/public/java.png" className="javaImage me-4" />
-              <h4 className="javaText">
-                Java e 🌱Spring Boot per lo sviluppo del backend, con particolare attenzione alla sicurezza e
-                all'architettura REST
-              </h4>
+
+            <Col xs={12} sm={6} md={3} lg={2}>
+              <div className="tech-card javaImg">
+                <img src="/public/java 2.png" alt="Java" className="tech-image mb-5" />
+                <div className="tech-overlay">
+                  <h4>Java per lo sviluppo del backend</h4>
+                </div>
+              </div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={2}>
+              <div className="tech-card">
+                <img src="/public/spring.png" alt="Spring" className="tech-image " />
+                <div className="tech-overlay">
+                  <h4>Spring e Spring Boot per la sicurezza e un'architettura REST manutenibile</h4>
+                </div>
+              </div>
             </Col>
           </Row>
         </Container>
-        <div className="">
-          <div>
+        <div>
+          <div className="">
             <CarouselLoghi />
           </div>
         </div>
