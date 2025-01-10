dd34895d40496458038283ce24088148d65e5462
Cecchy2 <dariocecchinato@gmail.com>
2025-01-10 11:05:54 +0100

    aggiunta logica per submit form manche tutta la logica per il submit, prima fare backend

--- src/components/FormSection.tsx
+++ src/components/FormSection.tsx
@@ -1,36 +1,88 @@
-import { Col, Container, Form, Image, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
 
 const FormSection = () => {
+  const [formValues, setFormValues] = useState({
+    nome: "",
+    cognome: "",
+    email: "",
+    messaggio: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <div className="FormSection">
+      <div className="formSection" id="formSection">
         <Container>
           <Row className="pt-5">
             <Col></Col>
           </Row>
           <Row className="pt-5">
             <Col md={5}>
-              <Image src="/photo-1432821596592-e2c18b78144f.avif" alt="macchina da scrivere" className="formImmage" />
+              <Image src="/photo-1432821596592-e2c18b78144f.avif" alt="macchina da scrivere" className="formImage" />
             </Col>
             <Col md={7} class>
               <h2 className="mt-5 display-6 fw-bold text-center">Lasciami un messaggio</h2>
-              <Form className="mt-5">
+              <Form className="my-5" onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formGroupNome">
                   <Form.Label>Nome</Form.Label>
-                  <Form.Control type="name" placeholder="inserisci il tuo nome" />
+                  <Form.Control
+                    type="text"
+                    name="nome"
+                    value={formValues.nome}
+                    onChange={handleChange}
+                    placeholder="Inserisci il tuo nome"
+                    required
+                  />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formGroupCognome">
                   <Form.Label>Cognome</Form.Label>
-                  <Form.Control type="name" placeholder="inserisci il tuo cognome" />
+                  <Form.Control
+                    type="text"
+                    name="cognome"
+                    value={formValues.cognome}
+                    onChange={handleChange}
+                    placeholder="Inserisci il tuo cognome"
+                    required
+                  />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formGroupEmail">
                   <Form.Label>Email </Form.Label>
-                  <Form.Control type="email" placeholder="Inserisci la tua email" />
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    value={formValues.email}
+                    onChange={handleChange}
+                    placeholder="Inserisci la tua email"
+                    required
+                  />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formGroupText">
                   <Form.Label>Scrivi il tuo messaggio</Form.Label>
-                  <Form.Control as="textarea" rows={5} />
+                  <Form.Control
+                    as="textarea"
+                    rows={5}
+                    name="messaggio"
+                    value={formValues.messaggio}
+                    onChange={handleChange}
+                    placeholder="Scrivi un messaggio..."
+                    required
+                  />
                 </Form.Group>
+                <Button variant="warning" className="w-100" type="submit">
+                  {" "}
+                  Invia
+                </Button>
               </Form>
             </Col>
           </Row>
