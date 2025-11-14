2c86884991d367e4f4235af0033dccbf0a0f3841
Dario Cecchinato <165845749+Cecchy2@users.noreply.github.com>
2025-11-15 00:24:02 +0100

    FIX

--- src/components/MyNavBar.tsx
+++ src/components/MyNavBar.tsx
@@ -8,25 +8,33 @@ const MyNavBar = () => {
   const handleClose = () => setExpanded(false);
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary navbarIndex" fixed="top" expanded={expanded}>
+    <Navbar
+      expand="lg"
+      className="bg-body-tertiary navbarIndex"
+      fixed="top"
+      expanded={expanded}
+    >
       <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={handleToggle}
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto nav-links">
             <Nav.Link href="#home" onClick={handleClose}>
               HOME
             </Nav.Link>
             <Nav.Link href="#about-me" onClick={handleClose}>
-              ABOUT ME
+              CHI SONO
             </Nav.Link>
             <Nav.Link href="#skills" onClick={handleClose}>
               SKILLS
             </Nav.Link>
             <Nav.Link href="#sicilyFresh" onClick={handleClose}>
-              MY WORKS
+              PROGETTI
             </Nav.Link>
             <Nav.Link href="#formSection" onClick={handleClose}>
-              CONTACTS
+              CONTATTAMI
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
